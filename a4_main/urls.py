789b41f71f605323ef8b75d3ff1213c9dd22a6a4
Sergiy Naumov <1414sergiy@gmail.com>
2021-05-28 17:13:13 +0300

    Create base.html file

--- a4_main/urls.py
+++ a4_main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.first_page_character_entry),
-    path('word_length', views.choice_word_length),
-    path('sorting_results', views.displaying_sorting_results),
-    path('meaning_words', views.explanation_meaning_words),
+    path('choice', views.choice_word_length),
+    path('sorting', views.displaying_sorting_results),
+    path('explanation', views.explanation_meaning_words),
 ]
--- a4_main/views.py
+++ a4_main/views.py
@@ -3,19 +3,19 @@ from django.shortcuts import render
 
 # Create a method to enter a string of values.
 def first_page_character_entry(request):
-    return render(request, 'a4_main/first_page_character_entry_template.html')
+    return render(request, 'a4_main/entry.html')
 
 
 # Create a method to determine the length of a word.
 def choice_word_length(request):
-    return render(request, 'a4_main/choice_word_length_template.html')
+    return render(request, 'a4_main/choice.html')
 
 
 # Create a method to displaying sorting results.
 def displaying_sorting_results(request):
-    return render(request, 'a4_main/displaying_sorting_results_template.html')
+    return render(request, 'a4_main/sorting.html')
 
 
 # Create a method to explanation of the meaning of words.
 def explanation_meaning_words(request):
-    return render(request, 'a4_main/explanation_meaning_words_template.html')
+    return render(request, 'a4_main/explanation.html')
